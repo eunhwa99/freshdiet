66308dfc876127b56c2127682f589566b0fc6ba8
eunhwa99 <dmsghkp@naver.com>
2021-03-26 10:44:29 +0900

    challenge 사진추가

--- app/src/main/java/com/example/freshdiet/challenge/ChallengeMain.java
+++ app/src/main/java/com/example/freshdiet/challenge/ChallengeMain.java
@@ -50,6 +50,7 @@ public class ChallengeMain extends AppCompatActivity implements ViewFlipperActio
         //UI
         flipper = (ViewFlipper)findViewById(R.id.flipper);
         index0 = findViewById(R.id.txtIndex0);
+        index0.setTextColor(Color.BLACK);
         index1 = findViewById(R.id.txtIndex1);
         index2 = findViewById(R.id.txtIndex2);
 
@@ -119,71 +120,4 @@ public class ChallengeMain extends AppCompatActivity implements ViewFlipperActio
 }
 
 
-
-   /* private void initListView(){
-        adapter=new ChallengeAdapter(listitem);
-
-        // 리스트뷰 참조 및 Adpater 달기
-        listView=(ListView)findViewById(R.id.challengelistview);
-        listView.setAdapter(adapter);
-
-        // listview에 클릭 이벤트 핸들러 정의
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-        });
-    }
-    private class ChallengeAdapter extends BaseAdapter{
-        private ArrayList<String> items;
-        public ChallengeAdapter(ArrayList<String> items){
-            this.items=items;
-        }
-
-        @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return items.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.M)
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            View v = view;
-            if (v == null) {
-                LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                v = vi.inflate(R.layout.challengelist, viewGroup,false);
-
-                v.setMinimumHeight(viewGroup.getHeight()/getCount());
-            }
-            ImageView imageView = (ImageView)v.findViewById(R.id.categoryiv);
-            if("숙면".equals(items.get(i))){
-                imageView.setImageResource(R.drawable.ic_baseline_nightlight_round_24);
-            }
-            else if("운동/건강".equals(items.get(i))){
-                imageView.setImageResource(R.drawable.ic_baseline_directions_run_24);
-            }
-            else if("식습관".equals(items.get(i))){
-                imageView.setImageResource(R.drawable.ic_baseline_flatware_24);
-            }
-            else{
-                imageView.setImageResource(R.drawable.ic_baseline_more_horiz_24);
-            }
-
-            TextView textView=(TextView)v.findViewById(R.id.categorytext);
-            textView.setText(items.get(i));
-
-            return v;
-        }
-    }*/
 
